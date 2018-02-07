695e9cd2990f57841e5711b7a33004a2cbeb3769
TanzidKaiser <tanzid.888@gmail.com>
2018-02-07 16:35:38 +0600

    demege product still run

---
+++ GalaxyVibesPos/Models/DamageProductReceive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalaxyVibesPos.Models
+{
+    public class DamageProductReceive
+    {
+        public int DamageProductID { get; set; }
+        public string DamageProductNo { get; set; }
+        public Nullable<int> CompanyID { get; set; }
+        public string DamageProductDate { get; set; }
+        public Nullable<int> SupplierID { get; set; }
+        public string InvoiceNo { get; set; }
+        public string DamageProductRemarks { get; set; }
+        public Nullable<int> DamageProductProductID { get; set; }
+        public Nullable<double> DamageProductPrice { get; set; }
+        public Nullable<double> DamageProductQuantity { get; set; }
+        public Nullable<double> DamageProductTotal { get; set; }
+    }
+}
